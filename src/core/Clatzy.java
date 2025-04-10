aeccb4183ee461e3941c98f33c37a5b2510fa81e
J-pater-06 <joelpaternina1211@gmail.com>
2025-04-09 19:48:47 -0500

    ese constructor no va

--- src/core/Clatzy.java
+++ src/core/Clatzy.java
@@ -17,12 +17,7 @@ public class Clatzy {
     private ArrayList<Curso> cursos;
     private ArrayList<Plan> planes;
 
-    public Clatzy() {
-        this.clientes = new ArrayList<>();
-        this.instructores =  new ArrayList<>();
-        this.cursos =  new ArrayList<>();
-        this.planes =  new ArrayList<>();
-    }
+    
     
     
     
